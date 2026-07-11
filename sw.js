e43ff80e9cd56a38d49be2510cb34eacebea5305
ashishapte2810 <ashishapte2810@gmail.com>
2026-07-10 21:14:05 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'court-tracker-v1';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(function(c) {
+      return c.addAll(['./tennis_tracker.html', './manifest.json', './icon.svg']);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+      return r || fetch(e.request);
+    })
+  );
+});
